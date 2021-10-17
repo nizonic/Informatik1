19f3ba2d2f73bf85a1bcdbe37a1b61fa4320e65c
Nico Manzoni <nico@menzipics.ch>
2021-10-17 20:51:51 +0200

    babui

--- Task4.1.py
+++ Task4.1.py
@@ -14,6 +14,6 @@ def is_prime(n):
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
-# can easily test many different values on every "Test & Run"!
+# can easily test.txt many different values on every "Test & Run"!
 print(is_prime(1291))
 
---
+++ temp.py
@@ -0,0 +1,2 @@
+with open("test.txt", "r") as babui:
+    print(babui.readlines())
