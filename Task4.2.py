19d90549ef875cfebfa1ecda867390de2aa82fc0
Nico Manzoni <nico@menzipics.ch>
2021-10-17 20:49:50 +0200

    Add files via upload

--- Task4.2.py
+++ Task4.2.py
@@ -2,17 +2,31 @@
 
 import os
 
+
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
 def get_average_grade(path):
     if not os.path.exists(path):
-        pass # what to do?
-    return -1
+        return None
+    grades = []
+    with open(path, "r") as file:
+        lines = file.readlines()
+        if not lines:  # empty file
+            return 0.0
+        for line in lines:
+            line = line.replace(" ", "")
+            if line == "\n" or line.startswith("#"):
+                continue  # skip this line
+            colon = line.index(":")
+            grade = line[colon + 1: -1]
+            grade = grade.strip()
+            if grade:  # check if grade value exists
+                grades.append(float(grade))
+    return sum(grades) / len(grades)
 
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
-# can easily test many different values on every "Test & Run"!
-print(get_average_grade("public/my_grades.txt"))
-
+# can easily test.txt many different values on every "Test & Run"!
+print(get_average_grade("./my_grades.txt"))
