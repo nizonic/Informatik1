2f69f99862f2da573aabf28debfb23b83ca00825
Nico Manzoni <nico@menzipics.ch>
2021-10-25 16:13:19 +0200

    Add files via upload

---
+++ Task5.1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# This signature is required for the automated grading to work.
+# Do not rename the function or change its list of parameters!
+
+def merge(a, b):
+    mergelist = []
+    if not a or not b:
+        return mergelist
+    lena = len(a)
+    lenb = len(b)
+    if lena >= lenb:
+        for x in range(lena):
+            if x >= lenb - 1:
+                mergelist.append((a[x], b[-1]))
+            else:
+                mergelist.append((a[x], b[x]))
+
+    if lenb > lena:
+        for x in range(lenb):
+            if x >= lena - 1:
+                mergelist.append((a[-1], b[x]))
+            else:
+                mergelist.append((a[x], b[x]))
+    return mergelist
+
+
+# The following line calls the function and prints the return
+# value to the Console. This way you can check what it does.
+# However, we encourage you to write tests, because then you
+# can easily test many different values on every "Test & Run"!
+print(merge([0, 1, 2], [5, 6]))
