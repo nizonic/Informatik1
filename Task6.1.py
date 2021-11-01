56364c858adb12cff946bc99634d08d68d7cb839
Nico Manzoni <nico@menzipics.ch>
2021-11-01 11:04:36 +0100

    Add files via upload

--- Task6.1.py
+++ Task6.1.py
@@ -3,10 +3,20 @@
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters.
 def read_csv(path):
-    pass
+    with open(path, "r") as file:
+        lines = file.readlines()
+        if not lines:
+            return []
+        df = []
+        for line in lines:
+            if line == "\n":
+                continue
+            df.append(tuple(line.strip().split(",")))
+        return df
+
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print(read_csv("public/example.csv"))
+print(read_csv("./example.csv"))
