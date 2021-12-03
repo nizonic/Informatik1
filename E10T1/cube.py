aa1ffd8a2effeb81b620aa40721def6b141032be
Nico Manzoni <nico@menzipics.ch>
2021-12-03 10:18:55 +0100

    cube culminated

--- E10T1/cube.py
+++ E10T1/cube.py
@@ -2,7 +2,7 @@ from geometric_object import GeometricObject
 
 
 class Cube(GeometricObject):
-    def __init__(self, side_length:float, color:str, filled:bool):
+    def __init__(self, side_length: float, color: str, filled: bool):
         super().__init__(color, filled)
         self.__side_length = side_length
 
@@ -10,7 +10,12 @@ class Cube(GeometricObject):
         return self.__side_length
 
     def get_area(self):
-        return 6 * self.__side_length**2
+        return 6 * self.__side_length ** 2
 
     def get_volume(self):
-        return self.__side_length**3
+        return self.__side_length ** 3
+
+
+if __name__ == "__main__":
+    c = Cube(6, "black", True)
+    print(c.get_volume())
