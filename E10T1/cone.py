588cc195abb4891083c0680d25296e78592055c0
Nico Manzoni <nico@menzipics.ch>
2021-12-03 10:09:56 +0100

    cone complete

--- E10T1/cone.py
+++ E10T1/cone.py
@@ -1,5 +1,30 @@
-from public.geometric_object import GeometricObject
+from geometric_object import GeometricObject
 
 
 class Cone(GeometricObject):
-    pass
+    def __init__(self, radius:float, vertical_height:float, lant_height:float, color:str, filled:bool):
+        super().__init__(color, filled)
+        self.__PI = 3.14
+        self.__radius = radius
+        self.__vertical_height = vertical_height
+        self.__slant_height = lant_height
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_vertical_height(self):
+        return self.__vertical_height
+
+    def get_slant_height(self):
+        return self.__slant_height
+
+    def get_area(self):
+        return self.__PI * self.__radius**2 + self.__PI * self.__radius * self.__slant_height
+
+    def get_volume(self):
+        return 1/3 * self.__PI * self.__radius**2 * self.__vertical_height
+
+
+if __name__ == "__main__":
+    c = Cone(2,5,3, "yellow", True)
+    print(c.get_color())
