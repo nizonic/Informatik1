ba7f56b2c7c04b60415efe6baa058ee7962fd030
Nico Manzoni <nico@menzipics.ch>
2021-12-03 10:09:43 +0100

    base class done

--- E10T1/geometric_object.py
+++ E10T1/geometric_object.py
@@ -1,5 +1,28 @@
 from abc import ABC, abstractmethod
 
 
-class GeometricObject:
-    pass
+class GeometricObject(ABC):
+    def __init__(self, color, filled):
+        self.color = color
+        self.filled = filled
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    @abstractmethod
+    def get_volume(self):
+        pass
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color:str):
+        self.color = color
+
+    def get_filled(self):
+        self.filled = True
+
+    def set_filled(self, filled:bool):
+        set.filled = filled
+
