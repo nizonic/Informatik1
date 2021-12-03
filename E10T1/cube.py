a103ab9579ce61fbc1c2d40063b48170f54f7956
Nico Manzoni <nico@menzipics.ch>
2021-12-03 10:16:17 +0100

    cylinder culminated

--- E10T1/cube.py
+++ E10T1/cube.py
@@ -2,4 +2,15 @@ from geometric_object import GeometricObject
 
 
 class Cube(GeometricObject):
-    pass
+    def __init__(self, side_length:float, color:str, filled:bool):
+        super().__init__(color, filled)
+        self.__side_length = side_length
+
+    def get_side_length(self):
+        return self.__side_length
+
+    def get_area(self):
+        return 6 * self.__side_length**2
+
+    def get_volume(self):
+        return self.__side_length**3
