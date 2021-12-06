952f9ccc2b1f4225a2450e6b2c321026870d8678
Nico Manzoni <nico@menzipics.ch>
2021-12-06 13:42:56 +0100

    finished electric_car

--- E10T4/electric_car.py
+++ E10T4/electric_car.py
@@ -9,16 +9,34 @@ from car import Car
 class ElectricCar:
 
     def __init__(self, battery_size, battery_range_km):
-        pass
+        self.__c_max = battery_size             #maximum capacity
+        self.__range = battery_range_km         #max range with full battery
+        self.__c = battery_size                 #current juice left
 
     def charge(self, kwh):
-        pass
+        if not isinstance(kwh, float): raise Warning
+        if kwh < 0: raise Warning
+        if self.__c + kwh > self.__c_max:
+            raise Warning("Overcharging the battery")
+        self.__c += kwh
 
     def get_battery_status(self):
-        pass
+        return (self.__c, self.__c_max)
 
     def get_remaining_range(self):
-        pass
+        return self.__range / self.__c_max * self.__c
 
     def drive(self, dist):
-        pass
+        if not isinstance(dist, float): raise Warning
+        if dist < 0: raise Warning
+        if dist > self.get_remaining_range():
+            self.drive(self.get_remaining_range())
+            raise Warning("Battery depleted")
+        self.__c -= self.__c_max / self.__range * dist
+
+
+if __name__ == "__main__":
+    e = ElectricCar(25.0, 500.0)
+    e.drive(100.0)
+    e.charge(2.0)
+    print(e.get_battery_status())  # (22.0, 25)
