1676773ff40e5e83395f69371c092c58702d9f4a
Nico Manzoni <nico@menzipics.ch>
2021-12-11 10:21:40 +0100

    finished random comprehensions

--- E12T1/data.py
+++ E12T1/data.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-with open("resource/words.txt") as f:
+with open("words.txt") as f:
     words = f.read().splitlines()
--- E12T1/script.py
+++ E12T1/script.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 
 from data import words
+from string import ascii_lowercase
+
 
 def words_with_length(length):
-    '''this one just serves as an example'''
     return [word for word in words if len(word) == length]
 
+
 def words_containing_string(s):
-    pass
+    return [word for word in words if word.find(s) > -1]
+
 
 def words_starting_with_character(c):
-    pass
+    return [word for word in words if word.startswith(c)]
+
 
 def alphabet():
-    '''you don't have to solve this one using a comprehension.'''
-    pass
+    return ascii_lowercase
+
 
 def dictionary():
-    pass
+    return {letter: words_starting_with_character(letter) for letter in ascii_lowercase}
+
 
 def censored_words(s):
-    pass
+    return ["x" * len(word) if s in word else word for word in words]
