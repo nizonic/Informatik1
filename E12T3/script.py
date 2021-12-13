5a0776190fb98056483ba87d71ed0d2849ae74da
Nico Manzoni <nico@menzipics.ch>
2021-12-13 15:41:59 +0100

    fuck difflib

--- E12T3/script.py
+++ E12T3/script.py
@@ -3,6 +3,7 @@
 import random
 import difflib
 
+
 class WordLogic(object):
 
     def __init__(self, num_words, len_words):
@@ -15,14 +16,13 @@ class WordLogic(object):
         return [word.upper() for word in word_list if len(word) is self.len_words]
 
     def word_selection(self):
-        #words = self.find_words_with_right_size()
-        #random.shuffle(words)
         words = []
         with open("words.txt") as f:
             word_list = f.read().splitlines()
-            words.append(random.choices(word_list, k = self.num_words // 3))
+            word_list = [word for word in word_list if len(word) == self.len_words]
+            words.extend(random.choices(word_list, k=self.num_words // 3))
             boss = random.choice(words)
-            while True:
+            for x in range(10000):
                 word = random.choice(word_list)
                 if word not in words and self.is_similar(boss, word, 0.4):
                     words.append(word)
@@ -30,15 +30,11 @@ class WordLogic(object):
                         break
             return words
 
-
-        # TODO: instead of returning a random sample of words, use the strategy described in task 2
-
-
     def is_similar(self, a, b, threshold):
         return difflib.SequenceMatcher(None, a, b).ratio() > threshold
 
 
 if __name__ == "__main__":
-    g = WordLogic(5, 5)
+    g = WordLogic(10, 7)
     print(g.is_similar("babui", "bababui", 0.8))
     print(g.word_selection())
